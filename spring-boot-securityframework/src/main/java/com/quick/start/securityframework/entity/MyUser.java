6c9212a256e268a2f5a9c1c45d1d1bb896f217ea
huang丶shen <1426887150@qq.com>
2022-06-17 09:04:20 +0000

    didi

---
+++ spring-boot-securityframework/src/main/java/com/quick/start/securityframework/entity/MyUser.java
@@ -0,0 +1,85 @@
+package com.quick.start.securityframework.entity;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 用户对象 sys_user
+ */
+public class MyUser extends BaseEntity<Integer> {
+
+    private static final long serialVersionUID = -6525908145032868837L;
+    private String userName;
+    private String password;
+    private String nickName;
+    private String phone;
+    private String email;
+    private Integer status;
+
+    public interface Status {
+        int LOCKED = 0;
+        int VALID = 1;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+            .append("userName", getUserName())
+            .append("nickName", getNickName())
+            .append("email", getEmail())
+            .append("phone", getPhone())
+            .append("password", getPassword())
+            .append("status", getStatus())
+            .append("createTime", getCreateTime())
+            .append("updateTime", getUpdateTime())
+            .toString();
+    }
+}
