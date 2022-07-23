c2bd91868eaa1285f5cd191802c01f1eb50f8408
huangshen <1426887150@qq.com>
2022-07-23 12:04:00 +0800

    添加前端内容

--- spring-boot-securityframework/build.gradle.kts
+++ spring-boot-securityframework/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies{
     implementation(platform(SpringBootPlugin.BOM_COORDINATES))
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
@@ -19,7 +20,15 @@ dependencies{
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("com.alibaba:fastjson:2.0.9")
+    // 验证码
     implementation("com.github.whvcse:easy-captcha:1.6.2")
+    // 浏览器信息工具类
+    implementation("eu.bitwalker:UserAgentUtils:1.21")
+    // IP
+    implementation("org.lionsoul:ip2region:2.6.5")
+    // 获取系统信息
+    implementation("com.github.oshi:oshi-core:6.2.2")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
---
+++ spring-boot-securityframework/src/main/java/com/quick/start/securityframework/aspectj/LogAspect.java
@@ -0,0 +1,63 @@
+package com.quick.start.securityframework.aspectj;
+
+import com.quick.start.securityframework.common.RequestHolder;
+import com.quick.start.securityframework.common.util.LogUtils;
+import com.quick.start.securityframework.common.util.SecurityUtils;
+import com.quick.start.securityframework.entity.MyLog;
+import com.quick.start.securityframework.service.MyLogService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Aspect
+public class LogAspect {
+    @Autowired
+    private MyLogService logService;
+
+    ThreadLocal<Long> currentTime = new ThreadLocal<>();
+    /**
+     * 设置操作日志切入点 记录操作日志 在注解的位置切入代码
+     */
+    @Pointcut("@annotation(com.quick.start.securityframework.annotation.MyLog)")
+    public void logPointCut() {
+    }
+
+    /**
+     * 配置环绕通知,使用在方法logPointcut()上注册的切入点
+     *
+     * @param joinPoint join point for advice
+     */
+    @Around("logPointCut()")
+    public Object saveSysLog(ProceedingJoinPoint joinPoint)throws Throwable{
+        Object result;
+        //记录方法的执行时间
+        currentTime.set(System.currentTimeMillis());
+        result = joinPoint.proceed();
+        //定义日志类型
+        MyLog log = new MyLog("INFO",System.currentTimeMillis() - currentTime.get());
+        currentTime.remove();
+        HttpServletRequest request = RequestHolder.getHttpServletRequest();
+        logService.save(SecurityUtils.getCurrentUsername(), LogUtils.getBrowser(request), LogUtils.getIp(request),joinPoint, log);
+        return result;
+    }
+
+    /**
+     * 配置异常通知
+     *
+     * @param joinPoint join point for advice
+     * @param e exception
+     */
+    @AfterThrowing(pointcut = "logPointCut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        MyLog log = new MyLog("ERROR",System.currentTimeMillis() - currentTime.get());
+        currentTime.remove();
+        log.setExceptionDetail(LogUtils.getStackTrace(e));
+        HttpServletRequest request = RequestHolder.getHttpServletRequest();
+        logService.save(SecurityUtils.getCurrentUsername(), LogUtils.getBrowser(request), LogUtils.getIp(request), (ProceedingJoinPoint)joinPoint, log);
+    }
+}
