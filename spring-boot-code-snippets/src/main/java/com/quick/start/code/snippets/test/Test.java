37e46e54ed2cc12be494b8d9fce1ba8623e70f23
huanghq01 <huanghq01@igoldenbeta.com>
2022-12-07 17:50:10 +0800

    提交测试代码

---
+++ spring-boot-code-snippets/src/main/java/com/quick/start/code/snippets/test/Test.java
@@ -0,0 +1,15 @@
+package com.quick.start.code.snippets.test;
+
+public class Test {
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 10; i++) {
+//            new Thread(() -> {
+//                if (true) {
+//                    continue;
+//                }
+//                // Do something
+//            }).start();
+//        }
+//
+//    }
+}
