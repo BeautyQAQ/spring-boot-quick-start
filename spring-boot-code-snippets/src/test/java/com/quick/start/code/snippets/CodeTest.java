c0b89f25bb272e25501c95bd64a822e252f30d53
huangshen <1426887150@qq.com>
2023-06-02 21:52:52 +0800

    添加code

--- spring-boot-code-snippets/src/test/java/com/quick/start/code/snippets/CodeTest.java
+++ spring-boot-code-snippets/src/test/java/com/quick/start/code/snippets/CodeTest.java
@@ -56,7 +56,7 @@ public class CodeTest {
     @Test
     public void testDate2(){
         String now = DateUtil.format(new Date(), "yyyyMMdd");
-//        String now = "20220708";
+        //String now = "20220708";
         System.out.println("now="+now);
         String year = StrUtil.sub(now, 0, 4);
         String month = StrUtil.sub(now, 4, 6);
@@ -107,15 +107,18 @@ public class CodeTest {
         tradingDayList.add("20210705");
         tradingDayList.add("20210708");
         // 取最后一个交易日, 以及前11个月的月末日期, 根据年月分组, 取最后一个
+        // 将这个 List 对象转换成一个 Map<String,String> 对象 tradingDayMap，根据年月分组。
+        // Map 的 key 为年月（即截取了 yyyyMMdd 中的前 6 位），value 为该年月中的最后一个交易日和前11个月的月末日期中的最大值。
         Map<String,String> tradingDayMap = tradingDayList.stream().collect(Collectors.groupingBy(e -> StrUtil.sub(e, 0, 6), Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(Integer::valueOf)), op->op.orElseThrow(()->new RuntimeException("获取交易日异常")))));
+        System.out.println(JSONUtil.toJsonStr(tradingDayMap));
         TreeMap<String, String> treeMap = new TreeMap<>(tradingDayMap);
         System.out.println(JSONUtil.toJsonStr(treeMap));
 
-//        List<Person> people = ...; // 假设这是一个存储了 Person 对象的列表
-//        Map<String, Integer> map = tradingDayList.stream()
-//                .collect(Collectors.toMap(
-//                        person -> person.getLastName() + "," + person.getFirstName(), // 指定如何提取键
-//                        Person::getAge // 指定如何提取值
-//                ));
+        // List<Person> people = ...; // 假设这是一个存储了 Person 对象的列表
+        // Map<String, Integer> map = tradingDayList.stream()
+        //         .collect(Collectors.toMap(
+        //                 person -> person.getLastName() + "," + person.getFirstName(), // 指定如何提取键
+        //                 Person::getAge // 指定如何提取值
+        //         ));
     }
 }
