78a104fb14b96f865f3d8a052e4256b5379eee25
huangshen <1426887150@qq.com>
2023-06-02 20:56:22 +0800

    添加

---
+++ spring-boot-auto-config/src/main/java/com/quick/start/autoconfig/SpringBootAutoConfig.java
@@ -0,0 +1,14 @@
+package com.quick.start.autoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoConfig.class, args);
+    }
+    
+
+}
