3f9caaf44eedd625bc63776b71f18a1dce75e04d
huangshen <1426887150@qq.com>
2023-06-04 15:11:26 +0800

    2

--- spring-boot-code-snippets/src/test/java/com/quick/start/code/snippets/CodeTest.java
+++ spring-boot-code-snippets/src/test/java/com/quick/start/code/snippets/CodeTest.java
@@ -13,6 +13,7 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
@@ -126,69 +127,65 @@ public class CodeTest {
     }
 
     @Test
-    public void testDate3(){
+    public void testDate3() {
+        // 集合流的过滤和映射
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .toList();
+        List<Integer> doubledEvenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 2)
+                .toList();
+        // 上述代码中，首先创建了一个包含整数的 List 对象 `numbers`。然后使用 `stream()` 方法将该 List 转换为一个流对象。接着使用 `filter` 方法过滤出其中的偶数，然后使用 `map` 方法将偶数都翻倍，最后用 `collect` 方法将结果收集到一个新的 List 中。
 
- //集合流的过滤和映射
+        // 对流进行分组和归约
+        List<Product> productList = new ArrayList<>();
+        Product a = new Product("a", 100.0);
+        Product b = new Product("b", 200.0);
+        productList.add(a);
+        productList.add(b);
+        Map<String, List<Product>> productsByCategory = productList.stream()
+                .collect(Collectors.groupingBy(Product::getCategory));
+        double averagePrice = productList.stream()
+                .collect(Collectors.averagingDouble(Product::getPrice));
+        double totalPrice = productList.stream()
+                .mapToDouble(Product::getPrice)
+                .sum();
+        Optional<Product> maxPriceProduct = productList.stream()
+                .max(Comparator.comparing(Product::getPrice));
+        // 上述代码中，假设从数据库中获取了一个产品列表，并定义了一个 `Product` 类型。可以使用 `groupingBy` 方法对产品按照类别进行分组，生成一个以类别名称为 key，对应产品列表为 value 的 Map 对象。使用 `averagingDouble` 方法获取产品价格的平均值，使用 `sum` 方法获取所有产品的总价。使用 `max` 方法获取产品中价格最高的那个产品。
 
-List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-List<Integer> evenNumbers = numbers.stream()
-                                    .filter(n -> n % 2 == 0)
-                                    .collect(Collectors.toList());
-List<Integer> doubledEvenNumbers = numbers.stream()
-                                            .filter(n -> n % 2 == 0)
-                                            .map(n -> n * 2)
-                                            .collect(Collectors.toList());
+        // 流的筛选操作
+        List<Product> expensiveProducts = productList.stream()
+                .filter(p -> p.getPrice() > 1000)
+                .toList();
+        boolean hasCheapProducts = productList.stream()
+                .anyMatch(p -> p.getPrice() < 50);
+        Optional<Product> firstCheapProduct = productList.stream()
+                .filter(p -> p.getPrice() < 50)
+                .findFirst();
+        // 上述代码中，可以使用 `filter` 方法筛选出价格超过 1000 的产品，并使用 `collect` 方法将结果收集到一个新的 List 中。可以使用 `anyMatch` 方法判断产品价格是否有低于 50 的，使用 `findFirst` 方法获取第一个价格低于 50 的产品。
 
-
-// 上述代码中，首先创建了一个包含整数的 List 对象 `numbers`。然后使用 `stream()` 方法将该 List 转换为一个流对象。接着使用 `filter` 方法过滤出其中的偶数，然后使用 `map` 方法将偶数都翻倍，最后用 `collect` 方法将结果收集到一个新的 List 中。
-
-//对流进行分组和归约
-
-List<Product> productList = new ArrayList<>();
-Map<String, List<Product>> productsByCategory = productList.stream()
-                                                            .collect(Collectors.groupingBy(Product::getCategory));
-double averagePrice = productList.stream()
-                                  .collect(Collectors.averagingDouble(Product::getPrice));
-double totalPrice = productList.stream()
-                                .mapToDouble(Product::getPrice)
-                                .sum();
-Optional<Product> maxPriceProduct = productList.stream()
-                                                .max(Comparator.comparing(Product::getPrice));
-
-
-// 上述代码中，假设从数据库中获取了一个产品列表，并定义了一个 `Product` 类型。可以使用 `groupingBy` 方法对产品按照类别进行分组，生成一个以类别名称为 key，对应产品列表为 value 的 Map 对象。使用 `averagingDouble` 方法获取产品价格的平均值，使用 `sum` 方法获取所有产品的总价。使用 `max` 方法获取产品中价格最高的那个产品。
-
-// 流的筛选操作
-
-List<Product> expensiveProducts = productList.stream()
-                                              .filter(p -> p.getPrice() > 1000)
-                                              .collect(Collectors.toList());
-boolean hasCheapProducts = productList.stream()
-                                      .anyMatch(p -> p.getPrice() < 50);
-Optional<Product> firstCheapProduct = productList.stream()
-                                                 .filter(p -> p.getPrice() < 50)
-                                                 .findFirst();
-
-
-//上述代码中，可以使用 `filter` 方法筛选出价格超过 1000 的产品，并使用 `collect` 方法将结果收集到一个新的 List 中。可以使用 `anyMatch` 方法判断产品价格是否有低于 50 的，使用 `findFirst` 方法获取第一个价格低于 50 的产品。
-
-//流对象的一些常见操作方法
-
-stream.forEach(System.out::println);   // 每个元素逐个打印
-stream.distinct();                     // 去除流中的重复元素
-stream.limit(5);                       // 高阶函数：只保留前 5 个元素
-stream.skip(5);                        // 高阶函数：跳过前 5 个元素
-stream.sorted();                       // 将流中的元素排序
-stream.toArray();                      // 将流中的元素转换为数组
-stream.reduce((a, b) -> a + b).get();  // 将流中的元素累加
-
-
-//上述代码中，`forEach` 方法用于对每个元素逐个执行某个操作；`distinct` 方法用于去除流中的重复元素；`limit` 和 `skip` 方法分别用于只保留前 n 个元素和跳过前 n 个元素；`sorted` 方法用于将流中的元素排序；`toArray` 方法用于将流中的元素转换为数组；`reduce` 方法用于在流中进行累加或者归约操作。
+        // 流对象的一些常见操作方法
+        // stream.forEach(System.out::println);   // 每个元素逐个打印
+        // stream.distinct();                     // 去除流中的重复元素
+        // stream.limit(5);                       // 高阶函数：只保留前 5 个元素
+        // stream.skip(5);                        // 高阶函数：跳过前 5 个元素
+        // stream.sorted();                       // 将流中的元素排序
+        // stream.toArray();                      // 将流中的元素转换为数组
+        // stream.reduce((a, b) -> a + b).get();  // 将流中的元素累加
+        // 上述代码中，`forEach` 方法用于对每个元素逐个执行某个操作；`distinct` 方法用于去除流中的重复元素；`limit` 和 `skip` 方法分别用于只保留前 n 个元素和跳过前 n 个元素；`sorted` 方法用于将流中的元素排序；`toArray` 方法用于将流中的元素转换为数组；`reduce` 方法用于在流中进行累加或者归约操作。
     }
 
-    private class Product {
+    private static class Product {
         private String category;
-        private String price;
+        private Double price;
+
+        public Product(String category, Double price){
+            this.category = category;
+            this.price = price;
+        }
 
         public String getCategory() {
             return category;
@@ -199,11 +196,11 @@ stream.reduce((a, b) -> a + b).get();  // 将流中的元素累加
             return this;
         }
 
-        public String getPrice() {
+        public Double getPrice() {
             return price;
         }
 
-        public Product setPrice(String price) {
+        public Product setPrice(Double price) {
             this.price = price;
             return this;
         }
